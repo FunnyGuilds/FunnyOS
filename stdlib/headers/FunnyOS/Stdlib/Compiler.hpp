67ca8eab5a7945dd464c20dd7af757dcaf1019a7
KrekBuk <register@mrgregorix.net>
2020-04-26 01:13:28 +0200

    Stdlib base: naked function specification

--- stdlib/headers/FunnyOS/Stdlib/Compiler.hpp
+++ stdlib/headers/FunnyOS/Stdlib/Compiler.hpp
@@ -9,6 +9,7 @@
 #   define F_CDECL                              __attribute__((__cdecl__))
 #   define F_SECTION(sectionName)               __attribute__((section(sectionName)))
 #   define F_UNUSED                             __attribute__((unused))
+#   define F_NAKED                              __attribute__((naked))
 
 // Struct alignment
 #   define F_DONT_ALIGN                         __attribute__((packed))
