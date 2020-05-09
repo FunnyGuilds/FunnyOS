719bcddf623fc28003567c526b70806b44b5befc
KrekBuk <register@mrgregorix.net>
2020-05-09 17:56:15 +0200

    Stdlib: Dynamic memory management via Owner and Ref

--- stdlib/headers/FunnyOS/Stdlib/Dynamic.hpp
+++ stdlib/headers/FunnyOS/Stdlib/Dynamic.hpp
@@ -5,6 +5,11 @@
 
 namespace FunnyOS::Stdlib {
 
+    namespace _Internal {
+        template <typename T>
+        struct DefaultDeleter;
+    }  // namespace _Internal
+
     /**
      * A SizedBuffer wrapper that will automatically allocate data on construction and free on destruction.
      *
@@ -30,6 +35,242 @@ namespace FunnyOS::Stdlib {
          */
         virtual ~SmartSizedBuffer();
     };
+
+    /**
+     * A smart pointer that is the sole owner of the contained object.
+     * Owner deletes the contained object when it goes out of scope.
+     *
+     * @tparam T type of the owned element.
+     * @tparam Deleter functor used to delete object contained in Owner
+     */
+    template <typename T, typename Deleter = _Internal::DefaultDeleter<T>>
+    class Owner {
+       public:
+        NON_COPYABLE(Owner);
+        MOVEABLE(Owner);
+
+        /**
+         * Constructs a new Owner that does not own anything.
+         */
+        Owner();
+
+        /**
+         * Constructs a new Owner that owns [ptr]
+         */
+        Owner(T* ptr);
+
+        /**
+         * Destructs the Owner and the object that is owns.
+         */
+        virtual ~Owner();
+
+        /**
+         * Destructs the object owned by this Owner and take the ownership of [ptr].
+         *
+         * @param ptr new object to take the ownership of (may be nullptr).
+         */
+        void Reset(T* ptr) noexcept;
+
+        /**
+         * Returns the pointer to the owned object and releases the ownership of it.
+         *
+         * @return the owned object (may be nullptr if the Owner did not own anything)
+         */
+        T* Release() noexcept;
+
+        /**
+         * Returns the pointer to the owned object.
+         *
+         * @return the owned object (may be nullptr if the Owner did not own anything)
+         */
+        T* Get() noexcept;
+
+        /**
+         * Returns the pointer to the owned object.
+         *
+         * @return the owned object (may be nullptr if the Owner did not own anything)
+         */
+        const T* Get() const noexcept;
+
+        /**
+         * Returns the pointer to the owned object.
+         *
+         * @return the owned object (may be nullptr if the Owner did not own anything)
+         */
+        T* operator->();
+
+        /**
+         * Returns the pointer to the owned object.
+         *
+         * @return the owned object (may be nullptr if the Owner did not own anything)
+         */
+        const T* operator->() const;
+
+       private:
+        Deleter m_deleter{};
+        T* m_data;
+    };
+
+    /**
+     * Constructs an object of type T and an Owner<T, Deleter> that owns T.
+     *
+     * @tparam T type of the object to create
+     * @tparam Args type of the arguments used to create instance of [T]
+     * @tparam Deleter deleter used to delete T
+     * @param args args to create an instance of [T]
+     * @return the newly created Owner owning the newly created [T] instance.
+     */
+    template <typename T, typename... Args, typename Deleter = _Internal::DefaultDeleter<T>>
+    Owner<T, Deleter> MakeOwner(Args&&... args);
+
+    namespace _Internal {
+        /**
+         * Reference counter helper.
+         */
+        class RefCounter {
+           public:
+            /**
+             * Constructs an uninitialized RefCounter.
+             */
+            inline RefCounter();
+
+            /**
+             * Initializes the ref counter and sets it counter to [1]
+             */
+            inline void Initialize();
+
+            /**
+             * If counter is initialized it is incremented by [1].
+             *
+             * @return copy of this counter.
+             */
+            inline RefCounter Acquire();
+
+            /**
+             * If counter is initialized it is decremented by [1].
+             *
+             * @return if this operation set the counter value to [0].
+             */
+            inline bool Release();
+
+            /**
+             * Invalidates this ref counter and makes it uninitialized.
+             */
+            inline void Invalidate();
+
+           private:
+            unsigned int* m_counter;
+        };
+    }  // namespace _Internal
+
+    /**
+     * A reference counting smart pointer. Multiple Refs may own the same object.
+     *
+     * When Ref goes out of scope, it is resettted or re-assigned it will delete the owned object ONLY if that Ref is
+     * the only owner of T.
+     *
+     * @tparam T type of the owned element
+     */
+    template <typename T>
+    class Ref {
+       public:
+        // TODO: Custom deleter, unit tests
+        COPYABLE(Ref);
+        MOVEABLE(Ref);
+
+        /**
+         * Constructs a new Ref that does not own anything.
+         */
+        Ref();
+
+        /**
+         * Constructs a new Ref that is the new sole owner of [ptr].
+         */
+        Ref(T* ptr);
+
+        /**
+         * Destructs the Ref.
+         *
+         * If this Ref is the last owner of the owned object, the owned object is also destroyed.
+         */
+        virtual ~Ref();
+
+        /**
+         * If this Ref is the last owner of the owned object, the owned object is destroyed.
+         * This Ref then takes ownership of [data].
+         *
+         * @param data new object to take the ownership of (may be nullptr, then now ownership is taken)
+         */
+        void Reset(T* data) noexcept;
+
+        /**
+         * Returns the pointer to the owned object.
+         *
+         * @return the owned object (may be nullptr if the Owner did not own anything)
+         */
+        T* Get() const noexcept;
+
+        /**
+         * Returns the pointer to the owned object.
+         *
+         * @return the owned object (may be nullptr if the Owner did not own anything)
+         */
+        T* operator->() const noexcept;
+
+       private:
+        template <typename U>
+        Ref(const Ref<U>& ref, T* ptr);
+
+        template <typename U>
+        Ref(Ref<U>&& ref, T* ptr);
+
+       private:
+        mutable _Internal::RefCounter m_refCounter;
+        T* m_data;
+
+       private:
+        template <typename U>
+        friend class Ref;
+
+        template <typename T2, typename U2>
+        friend Ref<T2> StaticRefCast(const Ref<U2>& ref);
+
+        template <typename T2, typename U2>
+        friend Ref<T2> StaticRefCast(Ref<U2>&& ref);
+    };
+
+    /**
+     * Constructs an object of type T and an Ref<T> that owns T.
+     *
+     * @tparam T type of the object to create
+     * @tparam Args type of the arguments used to create instance of [T]
+     * @tparam Deleter deleter used to delete T
+     * @param args args to create an instance of [T]
+     * @return the newly created Ref owning the newly created [T] instance.
+     */
+    template <typename T, typename... Args>
+    Ref<T> MakeRef(Args&&... args);
+
+    /**
+     * Creates a new instance of Ref<T> that owns the same object as [ref].
+     * Conversion from U* to T* is done via [ static_cast<T*>(ref.Get()) ].
+     *
+     * @param ref Ref to convert
+     * @return the converted ref.
+     */
+    template <typename T, typename U>
+    Ref<T> StaticRefCast(const Ref<U>& ref);
+
+    /**
+     * Creates a new instance of Ref<T> that owns the same object as [ref].
+     * Conversion from U* to T* is done via [ static_cast<T*>(ref.Get()) ].
+     * After calling this function [ref] is no longer in a valid state and should not be used.
+     *
+     * @param ref Ref to convert
+     * @return the converted ref.
+     */
+    template <typename T, typename U>
+    Ref<T> StaticRefCast(Ref<U>&& ref);
 
 }  // namespace FunnyOS::Stdlib
 
--- stdlib/headers/FunnyOS/Stdlib/Dynamic.tcc
+++ stdlib/headers/FunnyOS/Stdlib/Dynamic.tcc
@@ -6,6 +6,21 @@
 #define FUNNYOS_STDLIB_HEADERS_FUNNYOS_STDLIB_DYNAMIC_TCC
 
 namespace FunnyOS::Stdlib {
+    namespace _Internal {
+        template <typename T>
+        struct DefaultDeleter {
+            inline void operator()(T* ptr) {
+                delete ptr;
+            }
+        };
+
+        template <typename T>
+        struct DefaultDeleter<T[]> {
+            inline void operator()(T* ptr) {
+                delete[] ptr;
+            }
+        };
+    }  // namespace _Internal
 
     template <typename T>
     SmartSizedBuffer<T>::SmartSizedBuffer(const SmartSizedBuffer& other) {
@@ -60,6 +75,206 @@ namespace FunnyOS::Stdlib {
         Memory::FreeBuffer(*this);
     }
 
+    template <typename T, typename Deleter>
+    Owner<T, Deleter>& Owner<T, Deleter>::operator=(Owner<T, Deleter>&& other) noexcept {
+        if (&other == this) {
+            return *this;
+        }
+
+        Reset(other.m_data);
+        other.m_data = nullptr;
+        return *this;
+    }
+
+    template <typename T, typename Deleter>
+    Owner<T, Deleter>::Owner(Owner<T, Deleter>&& other) noexcept {
+        m_data = other.m_data;
+        other.m_data = nullptr;
+    }
+
+    template <typename T, typename Deleter>
+    Owner<T, Deleter>::Owner() : m_data(nullptr) {}
+
+    template <typename T, typename Deleter>
+    Owner<T, Deleter>::Owner(T* ptr) : m_data(ptr) {}
+
+    template <typename T, typename Deleter>
+    Owner<T, Deleter>::~Owner() {
+        Reset(nullptr);
+    }
+
+    template <typename T, typename Deleter>
+    void Owner<T, Deleter>::Reset(T* ptr) noexcept {
+        if (m_data != nullptr) {
+            m_deleter(m_data);
+        }
+
+        m_data = ptr;
+    }
+
+    template <typename T, typename Deleter>
+    T* Owner<T, Deleter>::Release() noexcept {
+        T* ptr = m_data;
+        m_data = nullptr;
+        return ptr;
+    }
+
+    template <typename T, typename Deleter>
+    T* Owner<T, Deleter>::Get() noexcept {
+        return m_data;
+    }
+
+    template <typename T, typename Deleter>
+    const T* Owner<T, Deleter>::Get() const noexcept {
+        return m_data;
+    }
+
+    template <typename T, typename Deleter>
+    T* Owner<T, Deleter>::operator->() {
+        return m_data;
+    }
+
+    template <typename T, typename Deleter>
+    const T* Owner<T, Deleter>::operator->() const {
+        return m_data;
+    }
+
+    template <typename T, typename... Args, typename Deleter>
+    Owner<T, Deleter> MakeOwner(Args&&... args) {
+        return Owner<T, Deleter>(new T(Forward<Args>(args)...));
+    }
+
+    namespace _Internal {
+        RefCounter::RefCounter() : m_counter{nullptr} {}
+
+        void RefCounter::Initialize() {
+            m_counter = new unsigned int{1};
+        }
+
+        RefCounter RefCounter::Acquire() {
+            if (m_counter == nullptr) {
+                return *this;
+            }
+
+            (*m_counter)++;
+
+            RefCounter counter;
+            counter.m_counter = m_counter;
+            return counter;
+        }
+
+        bool RefCounter::Release() {
+            if (m_counter == nullptr) {
+                return false;
+            }
+
+            return --(*m_counter) == 0;
+        }
+
+        void RefCounter::Invalidate() {
+            m_counter = nullptr;
+        }
+    }  // namespace _Internal
+
+    template <typename T>
+    Ref<T>& Ref<T>::operator=(const Ref<T>& other) {
+        if (&other == this) {
+            return *this;
+        }
+
+        Reset(nullptr);
+        m_refCounter = other.m_refCounter.Acquire();
+        m_data = other.m_data;
+
+        return *this;
+    }
+
+    template <typename T>
+    Ref<T>::Ref(const Ref<T>& other) : m_refCounter(other.m_refCounter.Acquire()), m_data(other.m_data) {}
+
+    template <typename T>
+    Ref<T>& Ref<T>::operator=(Ref<T>&& other) noexcept {
+        if (&other == this) {
+            return *this;
+        }
+
+        Reset(nullptr);
+        m_refCounter = other.m_refCounter;
+        m_data = other.m_data;
+        other.m_refCounter.Invalidate();
+        other.m_data = nullptr;
+
+        return *this;
+    }
+
+    template <typename T>
+    Ref<T>::Ref(Ref<T>&& other) noexcept : m_refCounter(other.m_refCounter), m_data(other.m_data) {
+        other.m_refCounter.Invalidate();
+        other.m_data = nullptr;
+    }
+
+    template <typename T>
+    Ref<T>::Ref() : m_refCounter{}, m_data{nullptr} {}
+
+    template <typename T>
+    Ref<T>::Ref(T* ptr) : m_refCounter{}, m_data{nullptr} {
+        Reset(ptr);
+    }
+
+    template <typename T>
+    Ref<T>::~Ref() {
+        Reset(nullptr);
+    }
+
+    template <typename T>
+    void Ref<T>::Reset(T* data) noexcept {
+        if (m_refCounter.Release()) {
+            delete m_data;
+            m_data = nullptr;
+        }
+        m_refCounter.Invalidate();
+
+        if (data != nullptr) {
+            m_refCounter.Initialize();
+            m_data = data;
+        }
+    }
+
+    template <typename T>
+    T* Ref<T>::Get() const noexcept {
+        return m_data;
+    }
+
+    template <typename T>
+    T* Ref<T>::operator->() const noexcept {
+        return m_data;
+    }
+
+    template <typename T>
+    template <typename U>
+    Ref<T>::Ref(const Ref<U>& ref, T* ptr) : m_refCounter{ref.m_refCounter.Acquire()}, m_data{ptr} {}
+
+    template <typename T>
+    template <typename U>
+    Ref<T>::Ref(Ref<U>&& ref, T* ptr) : m_refCounter{ref.m_refCounter}, m_data{ptr} {
+        ref.m_data = nullptr;
+        ref.m_refCounter.Invalidate();
+    }
+
+    template <typename T, typename... Args>
+    Ref<T> MakeRef(Args&&... args) {
+        return Ref<T>(new T(Forward<Args>(args)...));
+    }
+
+    template <typename T, typename U>
+    Ref<T> StaticRefCast(const Ref<U>& ref) {
+        return Ref<T>(ref, static_cast<T*>(ref.Get()));
+    }
+
+    template <typename T, typename U>
+    Ref<T> StaticRefCast(Ref<U>&& ref) {
+        return Ref<T>(Forward<U>(ref), static_cast<T*>(ref.Get()));
+    }
 }  // namespace FunnyOS::Stdlib
 
 #endif  // FUNNYOS_STDLIB_HEADERS_FUNNYOS_STDLIB_DYNAMIC_TCC
